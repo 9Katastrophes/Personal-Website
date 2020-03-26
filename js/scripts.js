296f985dfec2b382005a75ef9c5bd4b363cc5618
Katherine Wang <kathylu.wang@gmail.com>
2020-03-26 15:39:00 -0400

    Messed with links and jQuery. Added icons for LinkedIn and Github.

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+    $(".linkedin").click(function() {
+        window.open('https://www.linkedin.com/in/katherinelwang/', '_blank');
+    });
+    $(".github").click(function() {
+        window.open('https://github.com/9Katastrophes', '_blank');
+    });
+});
