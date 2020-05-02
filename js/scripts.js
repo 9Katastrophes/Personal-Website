f1d0a85900f2c2a6a4b7b92700f1380ba81a8734
Katherine Wang <kathylu.wang@gmail.com>
2020-05-01 22:26:03 -0400

    Worked on a lot of stuff. Filled in projects page.

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,12 @@
 $(document).ready(function() {
+    $('body').fadeIn(1000);
     $(".linkedin").click(function() {
         window.open('https://www.linkedin.com/in/katherinelwang/', '_blank');
     });
     $(".github").click(function() {
         window.open('https://github.com/9Katastrophes', '_blank');
     });
+    $(".facebook").click(function() {
+        window.open('https://www.facebook.com/9Katastrophes', '_blank');
+    })
 });
